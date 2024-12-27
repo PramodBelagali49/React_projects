328c588f0a0b885f68ae75d278b405b78cd1a00f
PramodBelagali49 <cse.pramod.b2@gmail.com>
2024-12-27 16:04:10 +0530

    Understanding basics of react

---
+++ 01basic_react/src/App.js
@@ -0,0 +1,11 @@
+import Chai from './Chai';
+
+function App() {
+  return (
+    <div>
+        {/* <h1>Hello Chai-aur-react learners!!</h1> */}
+        <Chai/>
+    </div>
+  )
+}
+export default App
---
+++ 01basic_react/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Chai is ready in basic react without vite</h2>
+    )
+}
+export default Chai;
